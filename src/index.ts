8e870050a743268fe989da410128bf31ca0da4e7
Bhargav Deshpande <bhargav.deshpande@mitwpu.edu.in>
2025-02-16 14:48:27 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,24 @@
 const handleScroll = () => {
-    const nav = document.querySelector('nav');
-    if (!nav) return;
-    if (window.scrollY > 0) {
-        nav.classList.add('bg-rose-500');
-        nav.classList.remove('bg-transparent');
-    } else {
-        nav.classList.add('bg-transparent');
-        nav.classList.remove('bg-rose-500');
-    }
-}
+  const nav = document.querySelector("nav");
+  if (!nav) return;
+  if (window.scrollY > 0) {
+    nav.classList.add("bg-rose-500");
+    nav.classList.remove("bg-transparent");
+  } else {
+    nav.classList.add("bg-transparent");
+    nav.classList.remove("bg-rose-500");
+  }
+};
 
-window.addEventListener('scroll', handleScroll);
+const toggleMobileMenu = (): void => {
+  const mobileMenu = document.getElementById("mobileMenu");
+  if (!mobileMenu) return;
+
+  if (mobileMenu.classList.contains("hidden")) {
+    mobileMenu.classList.remove("hidden");
+  } else {
+    mobileMenu.classList.add("hidden");
+  }
+};
+
+window.addEventListener("scroll", handleScroll);
